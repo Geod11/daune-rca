a49e9ef4ddd74db3464f3df9ec16b76b1d9de1b2
Geod11 <danilescugeorge@yahoo.com>
2025-01-27 01:03:54 +0200

    Cleaned comented code

--- script.js
+++ script.js
@@ -4,24 +4,9 @@ let isHamMenuExtended = false;
 const windowWidth = window.innerWidth;
 const facebookElement = document.querySelector("#facebookElement");
 
-const HeaderMobile = `
-
-`;
-const HeaderDesktop = `
-
-`;
-
 if (windowWidth < 700) {
   hamMenuOptions.style.display = "none";
 }
-
-// facebookElement.addEventListener(
-//   "click",
-//   window.open(
-//     "https://www.facebook.com/profile.php?id=61565389988489",
-//     "_blank"
-//   )
-// );
 
 function headerMenuDisplay() {
   if (isHamMenuExtended === false) {
@@ -31,24 +16,5 @@ function headerMenuDisplay() {
     hamMenuOptions.style.display = "none";
     isHamMenuExtended = false;
   }
-
-  // function wideScreenDisplay(windowWidth) {
-  //   if (windowWidth < 700)
-  //     windowWidth.hamMenuButton.addEventListener("change", windowWidth);
-  //   else {
-  //     hamMenuOptions.style.display = "flex";
-  //     isHamMenuExtended = false;
-  //     console.log(isHamMenuExtended);
-  //     console.log(windowWidth);
-  //   }
-  // }
-  // if (windowWidth < 700) {
-  //   headerMenuDisplay;
-  // } else {
-  //   hamMenuOptions.style.display = "flex";
-  //   isHamMenuExtended = false;
-  //   console.log(isHamMenuExtended);
-  //   console.log(windowWidth);
-  // }
 }
 hamMenuButton.addEventListener("click", headerMenuDisplay);
